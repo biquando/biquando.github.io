a10cf8973741cb0759474a33178e2e32b2a94503
biquando <biquando@gmail.com>
2019-12-29 14:08:13 -0800

    Remove .html extension

--- jsfiles/blinker.js
+++ jsfiles/blinker.js
@@ -3,7 +3,7 @@ var element = document.getElementById("blinker");
 function blink() {
     "use strict";
     element.style.opacity = element.style.opacity === "1" ? 0 : 1;
-    setTimeout(blink, element.style.opacity === "1" ? 650 : 350);
+    setTimeout(blink, element.style.opacity === "1" ? 250 : 250);
 }
 
 blink();
